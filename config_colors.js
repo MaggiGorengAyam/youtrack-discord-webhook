d08e8f63ad47b2ca49855d13ac282610c3423664
Donovan Charles <31787139+MaggiGorengAyam@users.noreply.github.com>
2023-01-12 14:21:51 +0800

    Update config_colors.js

--- config_colors.js
+++ config_colors.js
@@ -1,9 +1,8 @@
 module.exports = {
   	DEFAULT: "3093D1", // Will be used if no color is specified for an event
-    POSITIVE: "22C55E",
-    REGULAR: "3093D1",
-    NEGATIVE: "EF4444",
-  	WARNING: "FFEE58",
+    GREEN: "22C55E",
+    RED: "EF4444",
+  	YELLOW: "FFEE58",
   	GREY: "ABA99D",
   	PURPLE: "76278f",
 };
